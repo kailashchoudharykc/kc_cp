d344a90ba77dce6724a4943b388b636f3bdf4d75
kc_17 <kailashchoudharykc17@gmail.com>
2020-08-11 10:30:53 +0530

    kc

---
+++ test.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define sz(a) (ll) a.size()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (ll i = (a); i <= (b); ++i)
+#define oo 1e18
+#define pi pair<ll, ll>
+#define all(x) x.begin(), x.end()
+const ll mod = 998244353ll;
+const ll P = 1000000009ll;
+const ll N = 205ll;
+bool cmp(ll x, ll y)
+{
+  return x > y;
+}
+void solve()
+{
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+
+  ll t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+    cout << "\n";
+  }
+  return 0;
+}
